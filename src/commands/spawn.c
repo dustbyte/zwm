a966b9d157f79ed712ef3c276f22bf3cb31a7afb
Pierre Wacrenier <mota@souitom.org>
2010-07-01 01:54:35 +0200

    -Spawn

--- src/commands/spawn.c
+++ src/commands/spawn.c
@@ -1,11 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
-#include	"log.h"
-#include	"zwm.h"
+#include "log.h"
+#include "zwm.h"
+
+extern Wm	wm;
 
 void		spawn(const Arg *arg)
 {
-  (void)arg;
-  wlog(RUN | INFO, "SPAWN");
+  if (fork() == 0)
+    {
+      if (wm.dpy != NULL)
+	close(ConnectionNumber(wm.dpy));
+      setsid();
+      execvp(((char **) arg->args)[0], (char **) arg->args);
+      wlog(SYS | ERR, "can't start %s", ((char **) arg->args)[0]);
+      exit(0);
+    }
+  wlog(SYS | ERR, "fork failed, can't start %s", ((char **) arg->args)[0]);
 }
