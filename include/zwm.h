056788f8490b3d9f1a133ecc3a5bd2476e946f0a
Pierre Wacrenier <mota@souitom.org>
2010-07-01 18:02:37 +0200

    Fonction drawe

--- include/zwm.h
+++ include/zwm.h
@@ -10,53 +10,66 @@
 
 #define	TABLELENGTH(X) (sizeof((X)) / sizeof(*(X)))
 
-typedef struct _Client
+typedef struct		_Client
 {
-  t_elem	self;
-  Window	win;
-}		Client;
+  t_elem		self;
+  Window		win;
+}			Client;
 
-typedef struct	_Workspace
+typedef struct		_Workspace
 {
-  t_list	windows;
-  int		master_size;
-  Bool		stacked;
-  Client	*focus;
-  char		*repr;
-}		Workspace;
+  t_list		windows;
+  int			master_size;
+  Bool			stacked;
+  Client		*focus;
+  char			*repr;
+  struct _Layout	*layout;
+}			Workspace;
 
-typedef struct	_Conf
+typedef struct		_Conf
 {
-  double	master_width;
-  char		*border_focus;
-  char		*border_unfocus;
-}		Conf;
+  double		master_width;
+  char			*border_focus;
+  char			*border_unfocus;
+}			Conf;
 
-typedef struct	_Wm
+typedef struct		_Colors
 {
-  Workspace	*cur_workspace;
-  Workspace	*workspaces;
-  Conf		*conf;
-  Display	*dpy;
-  int		screen;
-  Window	root;
-  Bool		is_running;
+  unsigned int		focus;
+  unsigned int		unfocus;
+}			Colors;
 
-}		Wm;
-
-typedef union	_Arg
+typedef struct		_Wm
 {
-  int		val;
-  const void	*args;
-}		Arg;
+  unsigned int		cwrksp;
+  Workspace		*workspaces;
+  Conf			*conf;
+  Colors		colors;
+  Display		*dpy;
+  int			screen;
+  Window		root;
+  Bool			is_running;
+}			Wm;
 
-typedef struct	_Key
+typedef struct		_Layout
 {
-  unsigned int	mod;
-  KeySym	keysym;
-  void		(*func)(const Arg *arg);
-  Arg		arg;
-}		Key;
+  char			*name;
+  void			(*func)(Wm *wm);
+}			Layout;
+
+typedef union		_Arg
+{
+  int			val;
+  const void		*args;
+}			Arg;
+
+typedef struct		_Key
+{
+  unsigned int		mod;
+  KeySym		keysym;
+  void			(*func)(const Arg *arg);
+  Arg			arg;
+}			Key;
 
 /*
 ** Handlers
--- src/windows.c
+++ src/windows.c
@@ -15,22 +15,22 @@ void		add_window(Wm *wm, Window window)
   if ((win = malloc(sizeof(*win))) == NULL)
     wlog(SYS | ERR, "malloc");
   win->win = window;
-  list_add_head(&wm->cur_workspace->windows, &win->self, win);
-  wm->cur_workspace->focus = wm->cur_workspace->windows.head->data;
+  list_add_head(&wm->workspaces[wm->cwrksp].windows, &win->self, win);
+  wm->workspaces[wm->cwrksp].focus = wm->workspaces[wm->cwrksp].windows.head->data;
 }
 
 void		remove_window(Wm *wm, Client *win)
 {
-  if (win == wm->cur_workspace->focus)
+  if (win == wm->workspaces[wm->cwrksp].focus)
     {
       if (win->self.next != NULL)
-	wm->cur_workspace->focus = win->self.next->data;
+	wm->workspaces[wm->cwrksp].focus = win->self.next->data;
       else if (win->self.prev != NULL)
-	wm->cur_workspace->focus = win->self.prev->data;
+	wm->workspaces[wm->cwrksp].focus = win->self.prev->data;
       else
-	wm->cur_workspace->focus = NULL;
+	wm->workspaces[wm->cwrksp].focus = NULL;
     }
-  list_delete(&wm->cur_workspace->windows, &win->self, list_free_dfl);
+  list_delete(&wm->workspaces[wm->cwrksp].windows, &win->self, list_free_dfl);
 }
 
 Client		*get_window(Wm *wm, Window window)
@@ -38,7 +38,7 @@ Client		*get_window(Wm *wm, Window window)
   t_elem	*tmp;
   Client	*win;
 
-  list_foreach_as(wm->cur_workspace->windows.head, tmp, (Client *), win)
+  list_foreach_as(wm->workspaces[wm->cwrksp].windows.head, tmp, (Client *), win)
     if (win->win == window)
       return (win);
   return (NULL);
